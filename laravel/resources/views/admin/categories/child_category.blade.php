3ce281073f08ddd97c1f6cd272c83be82514af7b
GiaBao <nguyenbao.nta@gmail.com>
2022-11-09 11:48:44 +0700

    delete

--- laravel/resources/views/admin/categories/child_category.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach($subcategories as $subcategory)
-	@if($subcategory->id != $data->id)
-		<option value="{{$subcategory->id}}">-- {{$subcategory->name}}</option>
-		@if(count($subcategory->subCategories))
-			@include('admin.categories.child_category', ['subcategories' => $subcategory->subCategories])
-		@endif
-	@endif
-@endforeach
